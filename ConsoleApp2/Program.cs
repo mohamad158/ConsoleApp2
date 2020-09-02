27c87ff6877a9fc31b64f24dea237cb89e48e9c1
Mohamad Salameh <1930053@champlaincollege.qc.ca>
2020-09-02 15:24:58 -0400

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder("Hello");
+            sb.AppendLine(" 2nd Line");
+            sb.Append(".");
+
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
